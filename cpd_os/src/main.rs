194e15223fad46e3cc4fe1bd10c4506786a5dc9a
Hingee <nathan.hingee@gmail.com>
2026-03-25 17:00:21 +1100

    Layer 2 Complete, created safe println! and print! macro

--- cpd_os/src/main.rs
+++ cpd_os/src/main.rs
@@ -3,27 +3,17 @@
 
 use core::panic::PanicInfo;
 
-//static has actual mem location not inlined
-static HELLO: &[u8] = b"Hello World!";
-
-//const has NO actual mem location is inlined
-const VGA_BUFFER_STRT: u32 = 0xb8000;
+mod vga_buffer;
 
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() -> ! {
-    let vga_buf = VGA_BUFFER_STRT as *mut u8;
-
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buf.offset(i as isize * 2) = byte;
-            *vga_buf.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
-
+    println!("Hello World{}", "!");
+    panic!("Some panic message");
     loop {}
 }
 
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
     loop {}
 }
